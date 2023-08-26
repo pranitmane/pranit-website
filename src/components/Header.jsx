90bf682c72b9d12d879ad477fc201bbd17c7b6db
Pranit Mane <manepranit10@gmail.com>
2023-08-27 05:04:10 +0530

    assets issue fixed final

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
+import xLogo from '../assets/x-logo.svg'
+import githubLogo from '../assets/github-logo.svg'
+
 function Header() {
   return (
     <div className="w-screen h-16 bg-transparent flex justify-between">
       <div className="text-white m-4 font-logo text-2xl">Pranit Mane</div>
       <div className="text-white m-2 flex justify-center align-middle space-x-1">
         <a href="https://github.com/pranitmane" target="blank">
-          <img className="h-[40px]"src="/github-logo.svg"alt="github"/>
+          <img className="h-[40px]"src={githubLogo} alt="github"/>
         </a>
         <a href="https://twitter.com/pranitbmane" target="blank">
-          <img className="h-[40px] "src="/x-logo.svg"alt="twitter"/>
+          <img className="h-[40px] "src={xLogo} alt="twitter"/>
         </a>
       </div>
     </div>
